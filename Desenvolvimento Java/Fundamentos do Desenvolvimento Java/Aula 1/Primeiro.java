a2fc934b3e30acc9a3ec94f3accef17ab02c495b
Carol Alves <carol.alves99@live.com>
2021-04-21 08:28:07 -0300

    Aula 1

---
+++ Desenvolvimento Java/Fundamentos do Desenvolvimento Java/Aula 1/Primeiro.java
@@ -0,0 +1,9 @@
+public class Primeiro {
+	public static void main(String[] args) {
+		System.out.println("Nome: " + args[0]);
+		System.out.printf("Sobrenome: %s\n", args[1]);
+		System.out.println(args[2]);
+		System.out.println(args[3]);
+		System.out.println(args[4]);
+	}
+}
